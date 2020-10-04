e6ae328cfb3f4a3ed353f7c3a04c7a6b8ab0df38
SAMIA <68030713+lorjoh@users.noreply.github.com>
2020-10-04 07:37:39 -0400

    rearranging

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
     //create currentday and time
-    var currentDay = moment().format('dddd, MMMM Do'); // September 21st 2020
+    var currentDay = moment().format('dddd, MMMM Do');
     $("#currentDay").append(currentDay);
 
     var i = 0;
     var dayHours = 24;
     var blockContainerEl = $(".container");
+
+    var nowHour = moment().format('h A');
+    console.log(nowHour);
+   
+    // added 1 because of the nowHour which was included with fromNow statement
+    // var pastHours = moment().startOf('day').add(1, 'hour').fromNow('h');
+    // console.log(pastHours);
+    // subtracted 1 because of the nowHour which was included with fromNow statement
+    // var laterHours = moment().endOf('day').subtract(1, 'hour').fromNow('h');
+    // console.log(laterHours);
 
     for (; i < dayHours; i++) {
 
@@ -14,14 +24,14 @@ $(document).ready(function () {
         var dayHour = moment().startOf('day').add(i, 'hour').format('h A');
         console.log(dayHour);
         var everyHour = $("<h5>").addClass("hour col-md-2");
-        var nowHour = moment().format('h A');
-        console.log(nowHour);
 
-        var input = $("<h5>").addClass("description col-md-8");
+
+        var input = $("<input>").addClass("description col-md-8");
         input.textContent = "";
         if (nowHour === dayHour) {
             input.addClass("present");
         }
+
         if (dayHour < nowHour) {
             input.addClass("past");
         }
