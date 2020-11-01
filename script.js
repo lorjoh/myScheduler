46c464f0b5dd0a5939f30c1cf426050de3583094
SAMIA <68030713+lorjoh@users.noreply.github.com>
2020-10-31 21:30:15 -0400

    get not working

--- script.js
+++ script.js
@@ -7,79 +7,55 @@ $(document).ready(function () {
     var blockContainerEl = $(".container");
     var nowHour = moment().hour();
     console.log(nowHour);
-    var inputArray = [];
 
 
+    getInput();
 
+    for (; i < businessHours; i++) {
 
+        var eachHour = moment().startOf('day').hour(i + 8).hour();
+        console.log(eachHour);
+        var timeBlock = $("<div>").addClass("row").attr("data-index", i);
+        var hour = $("<h5>").addClass("hour col-md-2");
+        hour.append("<br>" + eachHour + ":00");
 
-    renderTimeBlocks();
-    // getInput();
-     
-
-    function renderTimeBlocks() {
-
-        for (; i < businessHours; i++) {
-
-            var eachHour = moment().startOf('day').hour(i + 8).hour();
-            console.log(eachHour);
-            var timeBlock = $("<div>").addClass("row").attr("data-index", i);
-            var hour = $("<h5>").addClass("hour col-md-2");
-            hour.append("<br>" + eachHour + ":00");
-
-            var inputEl = $("<input>").addClass("description col-md-8");
-
-            if (eachHour === nowHour) {
-                inputEl.addClass("present");
-            }
-            else if (eachHour < nowHour) {
-                inputEl.addClass("past");
-            }
-            else if (eachHour > nowHour) {
-                inputEl.addClass("future");
-            }
-
-            var saveBtn = $("<button>").addClass("saveBtn saveBtn,i:hover col-md-2");
-            var Icon = $("<i>").addClass("fas fa-save");
-            saveBtn.append(Icon);
-            timeBlock.append(hour, inputEl, saveBtn);
-            blockContainerEl.append(timeBlock);
+        var inputEl = $("<input>").addClass("description col-md-8");
+        inputEl.html("");
+        if (eachHour === nowHour) {
+            inputEl.addClass("present");
         }
+        else if (eachHour < nowHour) {
+            inputEl.addClass("past");
+        }
+        else if (eachHour > nowHour) {
+            inputEl.addClass("future");
+        }
+
+        var saveBtn = $("<button>").addClass("saveBtn saveBtn,i:hover col-md-2");
+        var Icon = $("<i>").addClass("fas fa-save");
+        saveBtn.append(Icon);
+        timeBlock.append(hour, inputEl, saveBtn);
+        blockContainerEl.append(timeBlock);
     }
 
 
-   function getInput() {
-
-        var savedInput = localStorage.getItem(time);
-
+    function getInput() {
+        var savedInput = localStorage.getItem(localStorage.key(i));
         if (savedInput !== null) {
-            // textInput = savedInput;
-            inputEl.html(savedInput);
+            $(".description").html(savedInput);
         }
-        else {
-            return;
-        }
-
-    }
+    };
 
 
     $(".saveBtn").on("click", function (event) {
-        // event.stopPropagation();
-        
         if (event.target.matches("button")) {
             event.preventDefault();
-
             var textInput = $(this).siblings(".description").val();
             var time = $(this).parent().attr("data-index");
-
             localStorage.setItem(time, textInput);
             getInput();
         }
-
-
-
     });
-
 });
 
 
